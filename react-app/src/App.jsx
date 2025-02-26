8bc2c6a08058dc6f9688f20cca3eded60ac0e74d
Madreeban0 <madreeban277@gmail.com>
2025-02-26 10:26:33 +0530

    home page added

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from '../src/Home';
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <Home /> 
+   </div>
+  )
+}
+
+export default App
---
+++ react-app/src/Home.jsx
@@ -0,0 +1,14 @@
+// import App from ".App.jsx";
+import './index.css';
+
+function Home(){
+    return(
+        <div className="home">
+            <h1>Welcome to my website</h1>
+            <p>Name: Reeban Austrive</p>
+            <p>Email: reeban.austrive.s76@kalvium.community</p>
+        </div>
+    )
+}
+
+export default Home;
